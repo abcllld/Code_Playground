3b61f3ef952360266878e45fff9ded5697ff44a0
abcllld <avaantivirus2021@gmail.com>
2023-02-17 07:42:31 +0530

    checking if an element is present or not in java Array

--- 2nd_Semester/Java/Arrays/Practice_Set/Set_02.java
+++ 2nd_Semester/Java/Arrays/Practice_Set/Set_02.java
@@ -3,6 +3,8 @@
 public class Set_02 {
     public static void main(String[] args) {
         int [] array = {76,54,88,33,21};
+        int num = 54;
+        boolean isPresent = false; //default is false
         for(int i=0;i<array.length;i++){
             if(){
 
